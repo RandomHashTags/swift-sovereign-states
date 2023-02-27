4fba78a6d8d87e84e528be51f68fcab2da7a91a3
RandomHashTags <ImRandomHashTags@gmail.com>
2023-02-27 14:40:51 -0600

    added some ducmentatino to `SovereignStateLevel2Division`

--- Sources/SwiftSovereignStates/SovereignStateLevel2Division.swift
+++ Sources/SwiftSovereignStates/SovereignStateLevel2Division.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 public protocol SovereignStateLevel2Division : SovereignRegion {
+    /// The level-1 administrative unit this division is located in or claimed by.
     var subdivision : any SovereignStateSubdivision { get }
+    /// _Federal Information Processing Standard_ number or regional equivalent representing this division.
     var fips_code : Int { get }
 }
 public extension SovereignStateLevel2Division {
     var cache_id : String {
         return subdivision.cache_id + "-" + rawValue
     }
+    /// _Federal Information Processing Standard_ number or regional equivalent representing this division as a string, formatted as `###`.
     var fips_code_string : String {
         return (fips_code < 10 ? "00" : fips_code < 100 ? "0" : "") + fips_code.description
     }
