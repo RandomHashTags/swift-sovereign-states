24d7e5eb59001a90a19082cf9d6f63126343daf2
RandomHashTags <ImRandomHashTags@gmail.com>
2023-02-28 12:58:08 -0600

    added `CountiesUnitedStatesRhodeIsland`

---
+++ Sources/SwiftSovereignStates/subdivisions/level_2/united_states/CountiesUnitedStatesRhodeIsland.swift
@@ -0,0 +1,30 @@
+//
+//  CountiesUnitedStatesRhodeIsland.swift
+//  
+//
+//  Created by Evan Anderson on 2/28/23.
+//
+
+import Foundation
+
+public enum CountiesUnitedStatesRhodeIsland : String, SovereignStateLevel2Division { // https://en.wikipedia.org/wiki/List_of_counties_in_Rhode_Island
+    case bristol
+    case kent
+    case newport
+    case providence
+    case washington
+    
+    public var subdivision : any SovereignStateSubdivision {
+        return SubdivisionsUnitedStates.rhode_island
+    }
+    
+    public var fips_code : Int {
+        switch self {
+        case .bristol: return 001
+        case .kent: return 003
+        case .newport: return 005
+        case .providence: return 007
+        case .washington: return 009
+        }
+    }
+}
