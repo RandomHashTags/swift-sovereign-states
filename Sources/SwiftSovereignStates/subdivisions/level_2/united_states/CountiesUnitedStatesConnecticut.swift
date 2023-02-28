c1f6ab5eb29a9870f500a2a1bbb51a47d7c2943f
RandomHashTags <ImRandomHashTags@gmail.com>
2023-02-28 13:50:52 -0600

    added `CountiesUnitedStatesConnecticut`

---
+++ Sources/SwiftSovereignStates/subdivisions/level_2/united_states/CountiesUnitedStatesConnecticut.swift
@@ -0,0 +1,36 @@
+//
+//  CountiesUnitedStatesConnecticut.swift
+//  
+//
+//  Created by Evan Anderson on 2/28/23.
+//
+
+import Foundation
+
+public enum CountiesUnitedStatesConnecticut : String, SovereignStateLevel2Division { // https://en.wikipedia.org/wiki/List_of_counties_in_Connecticut
+    case fairfield
+    case hartford
+    case litchfield
+    case middlesex
+    case new_haven
+    case new_london
+    case tolland
+    case windham
+    
+    public var subdivision : any SovereignStateSubdivision {
+        return SubdivisionsUnitedStates.connecticut
+    }
+    
+    public var fips_code : Int {
+        switch self {
+        case .fairfield: return 001
+        case .hartford: return 003
+        case .litchfield: return 005
+        case .middlesex: return 007
+        case .new_haven: return 009
+        case .new_london: return 011
+        case .tolland: return 013
+        case .windham: return 015
+        }
+    }
+}
