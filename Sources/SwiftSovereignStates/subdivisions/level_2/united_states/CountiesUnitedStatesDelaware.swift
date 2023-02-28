7455738b3bdb6d968eafca9e3ff70d2cd06d9698
RandomHashTags <ImRandomHashTags@gmail.com>
2023-02-28 13:06:40 -0600

    added `CountiesUnitedStatesDelaware`

---
+++ Sources/SwiftSovereignStates/subdivisions/level_2/united_states/CountiesUnitedStatesDelaware.swift
@@ -0,0 +1,26 @@
+//
+//  CountiesUnitedStatesDelaware.swift
+//  
+//
+//  Created by Evan Anderson on 2/28/23.
+//
+
+import Foundation
+
+public enum CountiesUnitedStatesDelaware : String, SovereignStateLevel2Division { // https://en.wikipedia.org/wiki/List_of_counties_in_Delaware
+    case kent
+    case new_castle
+    case sussex
+    
+    public var subdivision : any SovereignStateSubdivision {
+        return SubdivisionsUnitedStates.delaware
+    }
+    
+    public var fips_code : Int {
+        switch self {
+        case .kent: return 001
+        case .new_castle: return 003
+        case .sussex: return 005
+        }
+    }
+}
